2276c909b3872cebd8d71bc3511df495fc9abb99
JordiJacas <jjacasventura@iesesteveterradas.cat>
2018-01-29 18:35:02 +0100

    tutorial 3

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Movie;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +13,23 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        self::seedCatalog();
+  		$this->command->info('Tabla catálogo inicializada con datos!');
     }
+
+    public function seedCatalog()
+    {
+    	DB::table('movies')->delete();
+    	foreach( $this->arrayPeliculas as $pelicula ) {
+   			$p = new Movie;
+    		$p->title = $pelicula['title'];
+    		$p->year = $pelicula['year'];
+    		$p->director = $pelicula['director'];
+    		$p->poster = $pelicula['poster'];
+    		$p->rented = $pelicula['rented'];
+    		$p->synopsis = $pelicula['synopsis'];
+    		$p->save();
 }
+    }	
+}
+
